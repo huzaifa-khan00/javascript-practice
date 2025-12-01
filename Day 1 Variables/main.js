a5e99e61ed3d91f5bcc723b52f8f823e6ad928af
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-01 21:26:26 +0500

    Very Small Logic - Item Sold

---
+++ Day 1 Variables/main.js
@@ -0,0 +1,33 @@
+alert("Variables & Data Types")
+
+console.log("My first code in javascript");
+
+var firstName = "Huzaifa";
+
+var lastName = "Khan";
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+var fullName = lastName + " " +firstName;
+
+console.log(fullName);
+
+let item = "watch";
+
+let itemQty = 10;
+
+let itemSold;
+
+itemSold = --itemQty;
+console.log(itemSold);
+
+itemSold = --itemQty;
+console.log(itemSold);
+
+itemSold = --itemQty;
+console.log(itemSold);
+
+itemSold = --itemQty;
+console.log(itemSold);
