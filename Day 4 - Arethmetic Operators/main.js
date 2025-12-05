678bb34103aa36d46b22774125ed04e3e38b2217
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-05 12:26:43 +0500

    Arithmetic Operators

---
+++ Day 4 - Arethmetic Operators/main.js
@@ -0,0 +1,11 @@
+let a = 3;
+
+let b = 4;
+
+console.log("a = ", a, " & b = ", b );
+console.log("a + b =", a + b);
+console.log("b - a =", b - a);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a^b (a ** b or a exponent b =)", a ** b);
+console.log("a % b =", a % b);
