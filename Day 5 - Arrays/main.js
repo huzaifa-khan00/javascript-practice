0a209ab044af393c78fbeea153ed3bdfb05fafe8
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-14 23:11:01 +0500

    Array assignment without knowing the length

--- Day 5 - Arrays/main.js
+++ Day 5 - Arrays/main.js
@@ -1,6 +1,6 @@
-let array1 = ["atlanta", "baltimore", "denver", "chicago", "washington", "log angeles", "log-vegas"];
+let array1 = ["atlanta", "baltimore", "denver", "chicago", "washington", "los angeles", "los vegas"];
 
-let array1Length = array1.length;
+let array1Length = array1.length -3;
 
 array1.pop();
 
@@ -10,6 +10,7 @@ array1.splice(4, 3, "Karachi", "Lahore", "Islamabad");
 
 console.log(array1);
 
-let array2 = array1.slice(4, 7);
+let array2 = array1.slice(array1Length);
 
 console.log(array2);
+
