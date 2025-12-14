5c1a47e5b501d09f1964da2354c06f6ba5b98f8b
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-14 23:05:37 +0500

    Practice Assigment method - Arrays

--- Day 5 - Arrays/main.js
+++ Day 5 - Arrays/main.js
@@ -6,4 +6,10 @@ array1.pop();
 
 array1.push("Karachi");
 
+array1.splice(4, 3, "Karachi", "Lahore", "Islamabad");
+
 console.log(array1);
+
+let array2 = array1.slice(4, 7);
+
+console.log(array2);
