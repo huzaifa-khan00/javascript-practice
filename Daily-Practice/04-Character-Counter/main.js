dd544ecc17d952371f3bbee2a43707763602976c
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2025-12-16 10:27:00 +0500

    Add Input Validation

--- Daily-Practice/04-Character-Counter/main.js
+++ Daily-Practice/04-Character-Counter/main.js
@@ -1,5 +1,25 @@
-let userReview = prompt("Write your review");
+let userInput = prompt("Write your review");
 
-let reviewLength = userReview.length;
+let reviewLength = userInput.length;
 
-console.log(userReview, reviewLength);
+let maxLength = 200;
+
+let userReview = userInput;
+
+// Note: If we concatenate the string using "+", we get the number of characters in string rather than number.
+// But!, when separate with comma "," then we get the result for the characters left in Number (Must be a number),
+//keep in mind
+
+if(!userInput){
+    console.log("Invalid Input!");
+}
+else if(userInput.trim() === ""){
+    console.log("Cannot contain only spaces & can't be empty");
+}
+
+else if(userInput.length > 0 && userInput.length < 200){
+
+    let charLeft = reviewLength - maxLength;
+    console.log(userReview);
+    console.log("You've typed", reviewLength, "Characters", charLeft, "Characters Left.");
+}
