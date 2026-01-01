e22816be7692b7b1e8c76c24e7aa39bfbb7dc063
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-01 23:57:52 +0500

    Input Validation

--- Daily-Practice/07-Take-Biggest-value/main.js
+++ Daily-Practice/07-Take-Biggest-value/main.js
@@ -1,11 +1,17 @@
 var numbers = [10, 45, 23, 89, 12];
 
-var largest = numbers[0];
+if (numbers.length === 0) {
+    console.log("Array is empty");
+} else {
+    var largest = numbers[0];
 
-for (var i = 1; i < numbers.length; i++) {
-    if (numbers[i] > largest) {
-        largest = numbers[i];
+    for (var i = 1; i < numbers.length; i++) {
+        if (typeof numbers[i] === "number") {
+            if (numbers[i] > largest) {
+                largest = numbers[i];
+            }
+        }
     }
-}
 
-console.log("Largest number is:", largest);
+    console.log("Largest number is:", largest);
+}
