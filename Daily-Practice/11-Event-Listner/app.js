01b657a59eccc2ea7814997c19e5b277ccd659aa
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-07 22:17:36 +0500

    Color Changer Logic - Added

--- Daily-Practice/11-Event-Listner/app.js
+++ Daily-Practice/11-Event-Listner/app.js
@@ -1,6 +1,14 @@
-let colors = ["red", "green", "blue", "cyan", "crimson"];
+let colors = [];
 
 let greeting = prompt("Your Name");
+
+let colorRange = prompt("How many Colors");
+
+for(var i=0; i < colorRange; i++){
+    colors[i] = prompt("Color " + i+1);
+}
+
+console.log(colors);
 
 let span = document.getElementsByClassName('greeting')[0];
 
@@ -13,6 +21,18 @@ let buttonText = document.createElement('span');
 buttonText.innerText = "Button";
 buttonText.style.color = "red";
 
-heading.append(", Welcome to the color changer program!\nClick on any "+buttonText+" below to change it's color.");
+heading.append(", Welcome to the color changer program!\nClick on any ", buttonText," below to change it's color.");
 
-console.log(heading);
+let button1 = document.getElementsByClassName('button')[0];
+let button2 = document.getElementsByClassName('button')[1];
+let button3 = document.getElementsByClassName('button')[2];
+button1.addEventListener("click", colorChanger);
+button2.addEventListener("click", colorChanger);
+button3.addEventListener("click", colorChanger);
+
+function colorChanger(){
+    let randomColor = Math.floor(Math.random() * colors.length);
+    button1.style.backgroundColor = colors[randomColor];
+    button2.style.backgroundColor = colors[randomColor];
+    button3.style.backgroundColor = colors[randomColor];
+};
