64c3ebeac75de351e2337693a68124b805ff20fc
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-07 06:52:51 +0500

    Logic Fix

--- Daily-Practice/10-Practice-Exercise/main.js
+++ Daily-Practice/10-Practice-Exercise/main.js
@@ -1,9 +1,21 @@
 let childCount = document.getElementById('container').children.length;
 let container = document.getElementById('container');
 
+let success = false;
+
 document.getElementById("numOfChild").innerHTML = childCount;
 
 for(var i=0; i < childCount; i++){
-	container.children[i].style.fontSize = "20px";
+	container.children[i].style.fontSize = "30px";
 }
 
+for(var j=0; j < childCount; j++){
+    if(container.children[i] %2 === 0){
+        container.children[j].style.color = "red";
+        success = true;
+    };
+}
+
+if(success === false){
+    document.getElementById('note').style.display = "block";
+}
