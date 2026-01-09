23d959e727a0fcdd34a5860725d1bafdf30dcee0
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-10 00:17:11 +0500

    Hex Color Page

--- Daily-Practice/14-Color-Flipper/main.js
+++ Daily-Practice/14-Color-Flipper/main.js
@@ -2,11 +2,13 @@ let colors = ["red", "green", "blue", "rgba(255, 0, 200, 1)"];
 
 let btn = document.getElementById('btn');
 
-let color = document.getElementsByClassName('color');
+let color = document.getElementsByClassName('color')[0];
 
-let randomColor = Math.floor(Math.random() * colors.length);
+let bgColor = document.getElementsByTagName('main')[0];
+
 
 btn.addEventListener("click", function(){
-    document.body.style.backgroundColor = colors[randomColor];
-
+    let randomColor = Math.floor(Math.random() * colors.length);
+    bgColor.style.backgroundColor = colors[randomColor];
+    color.innerHTML = colors[randomColor];
 })
