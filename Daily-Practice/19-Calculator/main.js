dcc20de21761dd1ba50200a76d33dd7ed23567c1
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-15 05:06:32 +0500

    Logic Partially Completed

--- Daily-Practice/19-Calculator/main.js
+++ Daily-Practice/19-Calculator/main.js
@@ -1,4 +1,35 @@
+let equalCount = 0;
+
 function calculator(data){
-    console.log(data);
-    let currentText = document.getElementById('current-display').innerText += data;
+
+   if(data == "equal"){
+      equalCount++;
+
+      let result = document.getElementById('current-display').innerText;
+
+      let evalDisplay = document.getElementById('eval-display').innerText = result;
+
+      if(equalCount >= 2){
+         document.getElementById('eval-display-old').innerText = evalDisplay;
+      }
+      
+      result = document.getElementById('current-display').innerText = eval(result);
+      
+      console.log(result)
+      return;
+   }if(data == "clear"){
+      equalCount = 0;
+      document.getElementById('current-display').innerText = '';
+      document.getElementById('eval-display').innerText = '';
+      document.getElementById('eval-display-old').innerText = '';
+      return;
+   }if(data == "del"){
+      let del = document.getElementById('current-display').innerText;
+      del = del.slice(0, del.length -1);
+      document.getElementById('current-display').innerText = del;
+      return;
+   }
+
+   let currentData = document.getElementById('current-display').innerText += data;
+   
 }
