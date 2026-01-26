155758119116fde5f9637ec5ebc5bd5320fd9643
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-26 23:52:32 +0500

    feat: create logic for deposit amount

--- Daily-Practice/22-Bank-Management/main.js
+++ Daily-Practice/22-Bank-Management/main.js
@@ -1,6 +1,19 @@
 var initialAmount = 5000;
 var showResult = document.getElementById('showResult');
+var amountInput = document.getElementById('amount-inp');
 
 function checkAmount() {
     showResult.innerText = "Current amount: " + initialAmount;
+}
+
+function depositAmount() {
+    var amount = Number(amountInput.value);
+
+    if (amount < 500) {
+        alert("Please deposit at least 500");
+        return;
+    }
+
+    initialAmount = initialAmount + amount;
+    showResult.innerText = "Amount deposited: " + amount;
 }
