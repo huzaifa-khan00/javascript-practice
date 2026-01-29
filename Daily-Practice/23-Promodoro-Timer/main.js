aadb671bd031b2f57d6c1c67f49d56c4b861fcf0
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-29 14:10:53 +0500

    feat: add start and stop functionality

--- Daily-Practice/23-Promodoro-Timer/main.js
+++ Daily-Practice/23-Promodoro-Timer/main.js
@@ -1,34 +1,58 @@
-let minutesDisplay = document.getElementById('minutes');
-let secondsDisplay = document.getElementById('seconds');
-let dot = document.getElementById('dot');
+let minutesDisplay = document.getElementById("minutes");
+let secondsDisplay = document.getElementById("seconds");
+let dot = document.getElementById("dot");
 let interval;
-let minutes = 0;
-let seconds = 5;
+let minutes = 24;
+let seconds = 59;
+let isStopped = false;
+let startIsPressed = false;
 
-function start(){
+function start() {
+  if (isStopped === true) {
+    startIsPressed = true;
+    isStopped = false;
+  }
 
-    if(minutesDisplay.innerText === "Great Job!"){
+  if (startIsPressed === true) {
+    interval = setInterval(start, 1000);
+    startIsPressed = false;
+  }
+
+  if (minutesDisplay.innerText === "Great Job!") {
     clearInterval(interval);
-    return
+    return;
+  }
+
+  if (seconds === 1) {
+    minutes = minutes - 1;
+    seconds = 60;
+  }
+
+  seconds = seconds - 1;
+
+  if (minutes === 0 && seconds === 1) {
+    minutesDisplay.innerText = "Great Job!";
+    secondsDisplay.innerText = "🥳";
+    dot.innerText = "";
+    return;
+  }
+
+  if (minutes < 10) {
+    minutesDisplay.innerText = "0" + minutes;
+  }
+
+  if (seconds < 10) {
+    secondsDisplay.innerText = "0" + seconds;
+  }
+
+  minutesDisplay.innerText = minutes;
+  secondsDisplay.innerText = seconds;
 }
 
-    if(seconds === 1){
-        minutes = minutes-1;
-        seconds = 60;
-    }
+interval = setInterval(start, 1000);
 
-    seconds = seconds -1;
-
-        if(minutes === 0 && seconds === 1){
-        minutesDisplay.innerText = "Great Job!";
-        secondsDisplay.innerText = "🥳"
-        dot.innerText = '';
-        return
-    }
-    
-    minutesDisplay.innerText = minutes;
-    secondsDisplay.innerText = seconds;
-
+function stop() {
+  isStopped = true;
+  console.log(isStopped);
+  clearInterval(interval);
 }
-
-   interval = setInterval(start, 1000);
