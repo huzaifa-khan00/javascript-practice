1298aea09bb655246c5e40a6e4572054f3700130
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-01-31 00:26:28 +0500

    feat: add functionality to calculate tip

---
+++ Daily-Practice/24-Tip-Calculator/main.js
@@ -0,0 +1,10 @@
+let display = document.getElementById('display');
+let tipDisplay = document.getElementById('tip-display');
+
+function calculateTip(){
+    let bill = Number(document.getElementById('bill-input').value);
+    let tip = Number(document.getElementById('per-input').value);
+
+    display.innerText = eval(bill + (bill * (tip/100)));
+    tipDisplay.innerText = bill * (tip/100);
+}
