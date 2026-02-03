16d7e984f8cb237433414d72197fa940b34a936c
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-02-03 16:29:00 +0500

    feat: add logic to generate colored boxes

---
+++ Daily-Practice/25-Color-Box-Game/main.js
@@ -0,0 +1,17 @@
+let colors = ["#00F0FF", "#EC00FC", "#00CF35", "#007FFF", "#FF00FF",
+  "#00BFFF", "#00FF7F", "#EE82EE", "#1E90FF", "#40E0D0",
+  "#4B0082", "#4169E1", "#16E2F5", "#50C878", "#FF69B4",
+  "#15317E", "#00FF00", "#0047AB", "#00FA9A", "#800080",
+  "#007BA7", "#7FFF00", "#2554C7", "#DA70D6", "#BF00FF",
+  "#008080", "#7FFFD4", "#2B65EC", "#191970", "#8B008B"];
+let boxesContainer = document.getElementById('box-container');
+
+let boxAmount = Number(prompt("How many boxes"));
+
+  for (var i = 0; i <= boxAmount; i++){
+      let boxes = document.createElement('div');
+      boxes.className = 'game-box';
+      let randomColor = Math.floor(Math.random() * colors.length);
+      boxesContainer.appendChild(boxes);
+      boxes.style.backgroundColor = colors[randomColor];
+  }
