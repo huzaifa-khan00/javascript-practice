99dc10a8ebe0630838ee5e833d42878f6e9ac89b
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-02-16 13:06:23 +0500

    refactor: recreate the logic to add update & delete buttons to same element

--- Daily-Practice/28-To-Do-App/main.js
+++ Daily-Practice/28-To-Do-App/main.js
@@ -2,9 +2,9 @@ let parentContainer = document.getElementById('container');
 let userTask = document.getElementById('input');
 
 function create(){
-    let addNewTask = document.createElement('p');
-    addNewTask.id = 'new-task';
-    addNewTask.innerText = userTask.value;
-    parentContainer.appendChild(addNewTask);
-    console.log(userTask);
+    // let addNewTask = document.createElement('p');
+    // addNewTask.id = 'new-task';
+    // addNewTask.innerText = userTask.value;
+    parentContainer.innerHTML += `<div id="new-task-wrapper"><p id="new-task">${userTask.value}</p><div id="btn-wrapper"><button id="update">Update</button><button id="delete">Delete</button></div>`;
+    console.log(parentContainer.innerHTML);
 }
