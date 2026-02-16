53d2ee3743126bd5a8177984d9f0809a5d3a7dec
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-02-16 12:27:49 +0500

    feat: add functionality to add new task with smooth animation

--- Daily-Practice/28-To-Do-App/main.js
+++ Daily-Practice/28-To-Do-App/main.js
@@ -1,1 +1,10 @@
+let parentContainer = document.getElementById('container');
 let userTask = document.getElementById('input');
+
+function create(){
+    let addNewTask = document.createElement('p');
+    addNewTask.id = 'new-task';
+    addNewTask.innerText = userTask.value;
+    parentContainer.appendChild(addNewTask);
+    console.log(userTask);
+}
