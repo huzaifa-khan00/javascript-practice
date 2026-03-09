918f4370988aeee66bd68abdf3a8f732fe70d8c0
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-03-09 18:03:06 +0500

    refactor: seperate each job to increase performance through new function & show modal on history limit reach

---
+++ Daily-Practice/29-Roll-Dice-Simulator/main.js
@@ -0,0 +1,44 @@
+let dices = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
+let rollDice = document.getElementById("roll-dice-btn");
+let diceDisplay = document.getElementById("dice-display");
+let diceHistoryContainer = document.getElementById("dice-history-container");
+let modalWindow = document.getElementById("modal-container");
+let historyList = [];
+let html;
+let currentDice;
+
+generateRandomDice();
+
+function generateRandomDice() {
+  let randomDice = Math.floor(Math.random() * dices.length);
+  currentDice = dices[randomDice];
+  diceDisplay.innerText = currentDice;
+  updateHistory(currentDice);
+}
+
+function updateHistory(currentDice) {
+  historyList.push(currentDice);
+}
+
+function renderHistory() {
+  if (historyList.length >= 11) {
+    document.getElementById('text').innerText = "History Limit Reached!";
+    modalWindow.style.display = 'flex';
+    return;
+  }
+  html = "";
+  let counter = 1;
+  for (var i = 0; i < historyList.length; i++) {
+    html += `<div class="dice-history">
+       <span>Roll: ${counter++}</span><span style="font-size: 60px">${historyList[i]}</span>
+       </div>`;
+  }
+  diceHistoryContainer.innerHTML = html;
+  generateRandomDice();
+}
+
+function closeModal(){
+    modalWindow.style.display = 'none';
+}
+
+rollDice.addEventListener("click", renderHistory);
