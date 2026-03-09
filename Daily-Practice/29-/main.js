d94a61b20b8ff9a7de1ceaf3e71507240344677f
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-03-09 07:04:32 +0500

    feat: complete the project

--- Daily-Practice/29-/main.js
+++ Daily-Practice/29-/main.js
@@ -2,16 +2,27 @@ let dices = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
 let rollDice = document.getElementById('roll-dice-btn');
 let diceDisplay = document.getElementById('dice-display');
 let diceHistory = document.getElementById('dice-history-container');
-let counter = 0;
+let historyList = [];
+
+generateRandomDice();
+diceHistory.innerHTML = '';
+
+function renderHistory(){
+    diceHistory.innerHTML = '';
+    let counter = 1;
+    for(var i = 0; i < historyList.length; i++){
+        diceHistory.innerHTML += `<div class="dice-history">
+       <span>Roll: ${counter++}</span><span style="font-size: 60px">${historyList[i]}</span>
+       </div>`;
+    }
+    generateRandomDice();
+}
 
 function generateRandomDice(){
    let randomDice = Math.floor(Math.random() * dices.length);
    diceDisplay.innerText = dices[randomDice];
-   counter++
-   diceHistory.innerHTML += `<div class="dice-history">
-   <span>Roll: ${counter}</span><span>${diceDisplay.innerText}</span>
-   </div>`;
-   console.log(diceHistory.innerHTML);
+   historyList.push(diceDisplay.innerText);
+   console.log(historyList);
 };
 
-rollDice.addEventListener("click", generateRandomDice);
+rollDice.addEventListener("click", renderHistory);
