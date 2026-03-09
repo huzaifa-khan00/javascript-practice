f8050dffab5203ea40b7caa5d11139254a686a84
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-03-09 05:55:11 +0500

    feat: generate dice history

--- Daily-Practice/29-/main.js
+++ Daily-Practice/29-/main.js
@@ -1,8 +1,17 @@
 let dices = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
 let rollDice = document.getElementById('roll-dice-btn');
+let diceDisplay = document.getElementById('dice-display');
+let diceHistory = document.getElementById('dice-history-container');
+let counter = 0;
 
 function generateRandomDice(){
-    console.log(dices[0]);
+   let randomDice = Math.floor(Math.random() * dices.length);
+   diceDisplay.innerText = dices[randomDice];
+   counter++
+   diceHistory.innerHTML += `<div class="dice-history">
+   <span>Roll: ${counter}</span><span>${diceDisplay.innerText}</span>
+   </div>`;
+   console.log(diceHistory.innerHTML);
 };
 
 rollDice.addEventListener("click", generateRandomDice);
