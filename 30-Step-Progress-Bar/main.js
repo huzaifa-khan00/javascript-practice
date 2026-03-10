9a7bb4eac9b9e8fbf5fde62b8694171d1f689113
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-03-10 12:14:38 +0500

    feat: get initial data state

---
+++ 30-Step-Progress-Bar/main.js
@@ -0,0 +1,6 @@
+let progressBar = document.getElementById('progress-bar-front');
+let stepText1 = document.getElementsByClassName('step-text')[1];
+let stepText2 = document.getElementsByClassName('step-text')[2];
+let stepText3 = document.getElementsByClassName('step-text')[3];
+let stepText4 = document.getElementsByClassName('step-text')[4];
+
