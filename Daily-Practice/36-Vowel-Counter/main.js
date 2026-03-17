389bc641220411f301929f097892e2c470d499f6
huzaifa-khan00 <huzaifa.codes1@gmail.com>
2026-03-17 08:41:40 +0500

    feat: add vowel counter using nested loop

--- Daily-Practice/36-Vowel-Counter/main.js
+++ Daily-Practice/36-Vowel-Counter/main.js
@@ -3,22 +3,11 @@ let vowelCount = 0;
 let userInput = prompt("You type text, I count Vowels!. Sounds amazing right?");
 
 for (var i = 0; i < userInput.length; i++) {
-
-    if (userInput[i] === "a") {
-      vowelCount++;
-    }
-    if (userInput[i] === "e") {
-      vowelCount++;
-    }
-    if (userInput[i] === "i") {
-      vowelCount++;
-    }
-    if (userInput[i] === "o") {
-      vowelCount++;
-    }
-    if (userInput[i] === "u") {
-      vowelCount++;
-    }
+        for(var j = 0; j < vowels.length; j++){
+            if(userInput[i] === vowels[j]){
+                vowelCount++
+            }
+        }
   }
 
-console.log(`There are, ${vowelCount}, Vowels in ${userInput}`);
+console.log(`There are ${vowelCount} Vowels in ${userInput}`);
